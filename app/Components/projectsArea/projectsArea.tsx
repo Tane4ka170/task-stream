0d2c96441921de66bc547649c5abd6655af72074
Tane4ka170 <tane4ka170@gmail.com>
2025-09-26 12:55:31 +0300

    feat: Import/use ProjectsAreaHeader & TaskBoard

--- app/Components/projectsArea/projectsArea.tsx
+++ app/Components/projectsArea/projectsArea.tsx
@@ -1,11 +1,13 @@
 import { Card } from "@/components/ui/card";
 import React from "react";
+import ProjectsAreaHeader from "./projectsAreaHeader/ProjectsAreaHeader";
+import ProjectsAreaTaskBoard from "./ProjectsAreaBoards/ProjectsAreaTaskBoard";
 
 function ProjectsArea() {
   return (
     <Card className="shadow-none p-7 rounded-xl px-7 flex flex-col gap-8">
       <ProjectsAreaHeader />
-      <ProjectsAreaBoards />
+      <ProjectsAreaTaskBoard />
     </Card>
   );
 }
