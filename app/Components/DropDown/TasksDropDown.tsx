b34ad0b90498db9008375b42940527e492f0a5a8
Tane4ka170 <tane4ka170@gmail.com>
2025-09-27 21:05:33 +0300

    feat: Create reusable TasksDropDown component

---
+++ app/Components/DropDown/TasksDropDown.tsx
@@ -0,0 +1,55 @@
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { CircleX, Ellipsis, Pencil } from "lucide-react";
+import React from "react";
+
+type MenuItem = {
+  icon: JSX.Element;
+  label: string;
+  className: string;
+  separator?: undefined;
+};
+
+function TasksDropDown() {
+  const menuItems: MenuItem[] = [
+    {
+      icon: <Pencil />,
+      label: "Edit Task",
+      className: "",
+    },
+    {
+      icon: <CircleX />,
+      label: "Delete Task",
+      className: "text-red-200",
+    },
+  ];
+  return (
+    <div>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant={"ghost"} className="h-8 w-8 p-0">
+            <Ellipsis className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent>
+          {menuItems.map((item, index) =>
+            item.separator ? (
+              <DropdownMenuSeparator key={index} />
+            ) : (
+              <DropdownMenuItem key={index} className={``}></DropdownMenuItem>
+            )
+          )}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+}
+
+export default TasksDropDown;
